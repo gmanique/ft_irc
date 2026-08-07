2880f95cddae577760c9a74a8bbe44b0cabb662f
gmanique <gaetan5483@gmail.com>
2026-08-07 14:52:37 +0200

    feat: Topic command

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -42,6 +42,7 @@ class Server {
 		void								do_mode(Client &client, std::vector<std::string> &args);
 		int									do_part(Client &client, std::vector<std::string> &args);
 		int									do_kick(Client &client, std::vector<std::string> &args);
+		int									do_topic(Client &client, std::vector<std::string> &args);
 		int									quit(Client &client, std::vector<int> &fdsToClose, std::vector<std::string> &args);
 
 		int									do_invite(Client &client, std::vector<std::string> &args);
